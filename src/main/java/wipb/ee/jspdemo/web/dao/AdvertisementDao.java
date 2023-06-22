36217a8176bc60d060e353efb1a7a40fca3b21fd
kevvol <jakubkowalewski48@gmail.com>
2023-06-22 20:05:15 +0200

    Logs exists

--- src/main/java/wipb/ee/jspdemo/web/dao/AdvertisementDao.java
+++ src/main/java/wipb/ee/jspdemo/web/dao/AdvertisementDao.java
@@ -1,5 +1,6 @@
 package wipb.ee.jspdemo.web.dao;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -7,34 +8,61 @@ import jakarta.persistence.TypedQuery;
 import wipb.ee.jspdemo.web.model.Advertisement;
 import wipb.ee.jspdemo.web.model.Book;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 @Stateless
 public class AdvertisementDao {
+    private static final Logger logger = Logger.getLogger(AdvertisementDao.class.getName());
+    private static final String Logfile = "src/main/logs/logs.log";
+    @PostConstruct
+    public void init(){
+        try{
+            FileHandler fileHandler = new FileHandler(Logfile);
+            logger.addHandler(fileHandler);
+            logger.setLevel(Level.ALL);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.info("AdvertisementDao started working.");
+
+        }catch (IOException e){
+            throw new RuntimeException();
+        }
+    }
     @PersistenceContext(unitName = "PU")
     private EntityManager em;
 
     public Advertisement saveOrUpdate(Advertisement advertisement) {
-        if (advertisement.getId() == null)
+        if (advertisement.getId() == null) {
             em.persist(advertisement);
+            logger.info("Advertisement added:"+advertisement.getTitle());
+        }
         else {
             advertisement = em.merge(advertisement);
+            logger.info("Advertisement was updated:" + advertisement.getTitle());
         }
         return advertisement;
     }
 
     public void remove(Long id) {
         em.remove(em.getReference(Advertisement.class, id));
+        logger.info("Advertisement was removed:" + id.toString());
     }
 
     public Optional<Advertisement> findById(Long id) {
         Advertisement b = em.find(Advertisement.class,id);
+        logger.info("Advertisement was found:" + id.toString());
         return Optional.ofNullable(b);
     }
 
     public List<Advertisement> findAll() {
         TypedQuery<Advertisement> q = em.createNamedQuery("Advertisement.findAll", Advertisement.class);
+        logger.info("Advertisements returned");
         return q.getResultList();
     }
 
--- src/main/java/wipb/ee/jspdemo/web/dao/CategoryDao.java
+++ src/main/java/wipb/ee/jspdemo/web/dao/CategoryDao.java
@@ -1,5 +1,6 @@
 package wipb.ee.jspdemo.web.dao;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.ejb.Stateless;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
@@ -7,29 +8,58 @@ import jakarta.persistence.TypedQuery;
 import wipb.ee.jspdemo.web.model.Advertisement;
 import wipb.ee.jspdemo.web.model.Category;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 import java.util.List;
 import java.util.Optional;
 
 @Stateless
 public class CategoryDao {
+
+    private static final Logger logger = Logger.getLogger(CategoryDao.class.getName());
+    private static final String Logfile = "src/main/logs/logs.log";
+
+    @PostConstruct
+    public void init(){
+        try{
+            FileHandler fileHandler = new FileHandler(Logfile);
+            logger.addHandler(fileHandler);
+            logger.setLevel(Level.ALL);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.info("CategoryDao started working.");
+
+        }catch (IOException e){
+            throw new RuntimeException();
+        }
+    }
     @PersistenceContext(unitName = "PU")
     private EntityManager em;
 
     public Category saveOrUpdate(Category category) {
-        if (category.getId() == null)
+        if (category.getId() == null) {
             em.persist(category);
+            logger.info("New category added:" + category.getName());
+        }
         else {
             category = em.merge(category);
+            logger.info("Category edited:" + category.getName());
         }
         return category;
     }
 
     public void remove(Long id) {
         em.remove(em.getReference(Category.class, id));
+        logger.info("Category was deleted." + id.toString());
     }
 
     public Optional<Category> findById(Long id) {
         Category b = em.find(Category.class,id);
+        logger.info("Category was found" + id.toString());
         return Optional.ofNullable(b);
     }
 
@@ -38,11 +68,13 @@ public class CategoryDao {
                 .setParameter("name", name)
                 .getResultList();
         //em.close();
+        logger.info("Category was found" + name);
         return cat;
     }
 
     public List<Category> findAll() {
         TypedQuery<Category> q = em.createNamedQuery("Category.findAll", Category.class);
+        logger.info("Categories were returned");
         return q.getResultList();
     }
 
