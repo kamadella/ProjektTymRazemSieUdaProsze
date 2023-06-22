deb57dea21ba6ebfb31d047a0b34021bc8a4ec0e
Kamila Adamska <kamilaaadamska@gmail.com>
2023-06-22 16:13:39 +0200

    logowanie działa

---
+++ src/main/java/wipb/ee/jspdemo/web/controller/LoginController.java
@@ -0,0 +1,34 @@
+package wipb.ee.jspdemo.web.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet(name = "LoginController", urlPatterns = {"/login", "/login/admin"})
+public class LoginController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/login.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if("admin".equals(username) && "123".equals(password)){
+            HttpSession session = req.getSession();
+            session.setAttribute("isLoggedIn", true);
+            session.setAttribute("username", username);
+            resp.sendRedirect("/ee-jspdemo-web-1.0/advertisement/list"); //this page should be only acccessed after login
+        }else{
+            resp.sendRedirect("login?error=true");
+        }
+    }
+}
